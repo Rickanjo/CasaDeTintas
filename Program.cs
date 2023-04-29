f506b53d95fc56611d0dfb643417ad753eab03ec
Rickanjo <Ricardo.cassettari07@gmail.com>
2023-04-29 11:27:20 -0300

    Exercício Corrigido

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// Faça uma calculadora para uma casa de tintas. 
+Console.WriteLine("--- Casa de Tinta ---\n");
+
+const double LATA = 18;
+const double GALAO = 3.6;
+const double RENDIMENTO = 3;
+const double MARGEM = 1.1;
+
+
+
+Console.Write("Digite a área a ser pintada (m²)...: ");
+double area = double.Parse(Console.ReadLine()!);
+
+double tintaNecessaria = area / RENDIMENTO * MARGEM;
+int lataNecessaria = (int)Math.Ceiling(tintaNecessaria / LATA);
+int galoesNecessarios = (int)Math.Ceiling(tintaNecessaria / GALAO);
+
+Console.WriteLine($"\nQuantidade de tinta necessária.....: {tintaNecessaria} litro(s)\n");
+Console.WriteLine($"Número de latas necessária.........: {lataNecessaria} unidade(s)");
+Console.WriteLine($"Número de galões necessário........: {galoesNecessarios} unidade(s)");
